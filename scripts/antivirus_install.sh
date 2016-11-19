31c5e043385537435577bffcc6d54b23378b064e
Vishwajit Bhat <vishwajitbhat@gmail.com>
2016-11-19 15:55:21 -0800

    modified script

--- scripts/antivirus_install.sh
+++ scripts/antivirus_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 gcc ../antivirus_check.c -o /usr/bin/antivirus_scan
 gcc ../user.c -o /usr/bin/user
-nohup run_user.sh &
-sh ../install.sh
+nohup sh run_user.sh &
+cd ../
+sh install.sh
