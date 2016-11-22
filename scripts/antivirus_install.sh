d819cdc002d2ad402baa0f8bcf2d86947801f80c
Vishwajit Bhat <vishwajitbhat@gmail.com>
2016-11-21 20:39:18 -0800

    added new whitelist

--- scripts/antivirus_install.sh
+++ scripts/antivirus_install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-gcc ../antivirus_check.c -o /usr/bin/antivirus_scan
-gcc ../user.c -o /usr/bin/user
-nohup sh run_user.sh &
-cd ../
+dir=`uname -r`
+pwd=`echo linux-headers-${dir}`
+gcc /usr/src/${pwd}/LinuxAntivirus/antivirus_check.c -o /usr/bin/antivirus_scan
+gcc /usr/src/${pwd}/LinuxAntivirus/user.c -o /usr/bin/user
+nohup sh /usr/src/${pwd}/LinuxAntivirus/scripts/run_user.sh &
+cd /usr/src/${pwd}/LinuxAntivirus
 sh install.sh
