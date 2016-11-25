6a4836420be86be8d33f6abb68aa93ce4758bff9
Vishwajit Bhat <vishwajitbhat@gmail.com>
2016-11-24 18:16:15 -0800

    changes made to all scripts

--- scripts/sha1sum.sh
+++ scripts/sha1sum.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 files=$(find $1 -type d \( -path ./.git -o \
                   -path ./log -o \
                   -path ./public -o \
@@ -6,6 +8,8 @@ files=$(find $1 -type d \( -path ./.git -o \
 #echo $files
 for file in $files
 do
-    sha1sum $file >>/tmp/sha1sum.txt
-     #echo $file
+blacklist_sha1=`sha1sum $file`
+#echo $blacklist_sha1
+IFS=' ' read -ra blacklist_sha1Arr <<< "$blacklist_sha1"
+echo ${blacklist_sha1Arr[0]} >>/tmp/sha1sum.txt
 done
